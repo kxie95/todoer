31ad51e14bdbaacb1a322c88e6fc85451266e032
MYOB\Karen.Xie <karen.xie@myob.com>
2017-02-17 08:23:37 +1300

    Added TaskItem

---
+++ ToDoList/Models/TaskItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class TaskItem
+    {
+        public int TaskItemID { get; set; }
+        public DateTime ScheduledTime { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+
+    }
+}
